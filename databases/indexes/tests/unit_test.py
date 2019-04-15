5ac2b934620fa3a96ceb04afb07d2bd7ab7df481
James Lee <james.lee@jet.com>
2019-04-14 22:05:54 -0400

    hw3 final

--- databases/indexes/tests/unit_test.py
+++ databases/indexes/tests/unit_test.py
@@ -24,8 +24,9 @@ def load(fn):
 
     return result, cols
 
-def test_insert_and_import():
-    print("\n\n*************TEST ONE, TEST LOAD FUNCTIOn**************")
+def test_insert_and_import_data():
+    print("\n\n*************TEST ONE, TEST LOAD FUNCTION**************")
+    print("*** HERE WE TEST THE IMPORT_DATA FUNCTION WHICH LEVEREAGES INSERT. LATER WE ALSO TEST LOADIT = TRUE AND THE LOAD FUNCTION***")
     rows, cols = load("CSVFile/people.csv")
     t=CSVDataTable.CSVDataTable(table_name = "People",column_names = cols, primary_key_columns=['playerID'])
     t.import_data(rows)
@@ -34,9 +35,13 @@ def test_insert_and_import():
 def test_find():
     print("\n\n*************TEST TWO,  FIND BY TEMPLATE AND COMPARE RUN TIME WITH INDEX vs WITHOUT**************")
     tmp = {"playerID":"willite01"}
+    rows, cols = load("CSVFile/people.csv")
+    t=CSVDataTable.CSVDataTable(table_name = "People",column_names = cols, primary_key_columns=['playerID'])
+    t.import_data(rows)
 
+    print("\n***TESTING RUN WITHOUT INDEX***")
     start = time.time()
-    for i in range(0,1000):
+    for i in range(0,5):
         r = t.find_by_template(tmp,fields=None,use_index=False)
         if i ==0:
             print("Row = ",r)
@@ -44,8 +49,9 @@ def test_find():
     elapsed = end - start
     print("Elapsed time=", elapsed)
 
+    print("\n***TESTING RUNNING WITH INDEX***")
     start = time.time()
-    for i in range(0,1000):
+    for i in range(0,5):
         r=t.find_by_template(tmp,fields=None,use_index=True)
         if i==0:
             print("Row = ",r)
@@ -110,7 +116,7 @@ def test_duplicate_index_name():
 
 def test_incomplete_index_info():
     ###CHECK FOR not complete index info ADD_INDEX FUNCTION 
-    print("\n\n*************TEST SIX, CREATE WITH INDEX AND ADD ANOTHER INDEX**************")
+    print("\n\n*************TEST EIGHT, NOT ENOUGH INFORMATION TO CREATE INDEX THROWS AN ERROR**************")
     rows, cols = load("CSVFile/offices.csv")
     t=CSVDataTable.CSVDataTable(table_name = "Offices",column_names = cols, primary_key_columns=["officeCode"])
     cols = ["city","state","postalCode"]
@@ -118,6 +124,47 @@ def test_incomplete_index_info():
         t.add_index("location",cols)
     except Exception as e:
         print("\n We caught an error, you did  not provide enough information to create the key" + "\n",e)
+
+def test_delete():
+    print("\n\n*************TEST NINE, DELETE WORKS SUCCESSFULLY**************")
+    rows, cols = load("CSVFile/offices.csv")
+    t=CSVDataTable.CSVDataTable(table_name = "Offices",column_names = cols, primary_key_columns=["officeCode"])
+    t.import_data(rows)
+    print("\n **ORIGINAL META DATA BEFORE WE RUN DELETE, 10 ROWS**\n")
+    print(t)
+    
+    print("\n **DELETING CAMBRIDGE**\n")
+    r = t.delete({"city":"Cambridge"})
+
+
+    print("\n **DATA AFTER DELETING CAMBRIDGE, 9 ROWS**\n")
+    print(t)
+
+def test_save_and_load():
+    print("\n\n*************TEST TEN, SAVE AND LOAD**************")
+    rows, cols = load("CSVFile/offices.csv")
+
+    print("\n ***READING CSV TO LOAD OFFICES.CSV")
+    t=CSVDataTable.CSVDataTable(table_name = "Offices",column_names = cols, primary_key_columns=["officeCode"])
+    t.import_data(rows)
+
+    print("\n ***SAVING DOWN THE CSVDATATABLE")
+    t.save()
+
+
+    print("TABLE INFO AND ITS INDEXES\n")
+    print(t)
+    for k,v in t._indexes.items():
+        print(v.to_json())
+
+    print("\n ***LOADING CSV DATATABLE***")
+    t2 = CSVDataTable.CSVDataTable(table_name = "Offices",loadit=True)
+
+    print("\n **TABLE LOADED CONFIRM THEY ARE THE SAME**\n")
+    print(t2)
+    print("\n ***CONFIRM INDEX INTEGRITY***")
+    for k,v in t2._indexes.items():
+        print(v.to_json())
 
 
 def test_specific_project():
@@ -129,9 +176,10 @@ def test_specific_project():
     p = ["playerID","People.nameLast","Batting.H"]
     print("Specific template = ", t._get_specific_project())
 
+
 def test_join():
     ###CHECK FOR not complete index info ADD_INDEX FUNCTION 
-    print("*************TEST SIX JOIN AND ALSO SEE THAT IT SWAPS TABLESX**************")
+    print("\n\n*************TEST ELEVEN JOIN AND ALSO SEE THAT IT SWAPS TABLESX**************")
     rows, cols = load("CSVFile/People.csv")
     t=CSVDataTable.CSVDataTable(table_name = "People",column_names = cols, primary_key_columns=["playerID"])
     t.import_data(rows)
@@ -143,31 +191,42 @@ def test_join():
     print("T2 = ",t2)
 
     j = t2.join(t, ['playerID'],
-        w_cluase ={"People.nameLast":"Williams", "People.birthCity": "San Diego"},
+        w_clause ={"People.nameLast":"Williams", "People.birthCity": "San Diego"},
         p_clause =['playerID',"People.nameLast","People.nameFirst","Batting.teamID","Batting.yearID","Batting.stint","Batting.H","Batting.AB"]
         ,optimize = True)
 
     print("Result = " ,j)
     print("All rows = ", json.dumps(j._rows, indent=2))
 
-
-def test_delete():
-    print("*************TEST SIX, CREATE WITH INDEX AND ADD ANOTHER INDEX**************")
-    rows, cols = load("CSVFile/offices.csv")
-    t=CSVDataTable.CSVDataTable(table_name = "Offices",column_names = cols, primary_key_columns=["officeCode"])
-
+def test_delete_multiple_conditions():
+    print("\n\n*************TEST TWELEVE WE DELETE WITH MULTIPLE WHERE CONDITIONS AND ON A LARGER DB**************")
+    rows, cols = load("CSVFile/Batting.csv")
+    t=CSVDataTable.CSVDataTable(table_name = "Batting",column_names = cols, loadit= False)
     t.import_data(rows)
 
-    r = t.delete({"city":"Cambridge"})
+    print("\n **ORIGINAL META DATA BEFORE WE RUN DELETE**\n")
+    print(t)
+    
+    print("\n **DELETING yearID = 2017 and teamID = BOS FOR ALL BATTING RECORDS, 50 LESS ROWS**\n")
+
+    r = t.delete({"yearID":"2017", "teamID":"BOS"})
+
+
+    print("\n **DATA AFTER DELETING*\n")
     print(t)
 
 
-
-test_insert_and_import()
+test_insert_and_import_data()
+test_find()
 test_init()
 test_index()
 test_conflict()
 test_add_index()
 test_duplicate_index_name()
 test_incomplete_index_info()
+test_delete()
+test_save_and_load()
+test_join()
+test_delete_multiple_conditions()
+
 
