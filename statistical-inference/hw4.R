71447eee56f5f1f626135df2d0585fa27be4daf2
James Lee <james.lee@jet.com>
2019-04-24 22:22:28 -0400

    stats

--- statistical-inference/hw4.R
+++ statistical-inference/hw4.R
@@ -1,2 +1,7 @@
-cigrettes <- read.table('cigarettes.txt', header = T)
-hist(cigrettes$cigs)
+df<-read.table('/Users/james.lee/AnacondaProjects/academic_projects/statistical-inference/cigarettes.txt', header = TRUE)
+library(mgcv)
+fit_gam <- gam(cigs ~ educ+s(cigpric)+white+s(age)+s(income)+restaurn, data=df)
+summary(fit_gam)
+
+
+summary(lm(cigs~., data=df))
