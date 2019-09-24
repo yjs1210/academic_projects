0021970167f2b5ebd6c54fb60c822f3040e6b203
James Lee <yjs1210@gmail.com>
2019-09-23 20:06:23 -0400

    databases
    databases

--- statistical-inference/hw4.R
+++ statistical-inference/hw4.R
@@ -1,7 +1,14 @@
-df<-read.table('/Users/james.lee/AnacondaProjects/academic_projects/statistical-inference/cigarettes.txt', header = TRUE)
+df<-read.table('/Users/james/AnacondaProjects/academic_projects/statistical-inference/cigarettes.txt', header = TRUE)
 library(mgcv)
-fit_gam <- gam(cigs ~ educ+s(cigpric)+white+s(age)+s(income)+restaurn, data=df)
+fit_gam <- gam(cigs ~ educ+s(cigpric)+white+s(age)+s(income)+restaurn, data=df,family =poisson)
 summary(fit_gam)
+plot(fit_gam)
 
+fit_gam$coefficients
 
 summary(lm(cigs~., data=df))
+
+head(df)
+
+predict(fit_gam, newdata = data.frame(educ=12,cigpric=40,white=1,age=50,income=10000,cigs=3,restaurn=0))
+predict(fit_gam, newdata = data.frame(educ=12,cigpric=60,white=1,age=50,income=10000,cigs=3,restaurn=0))
